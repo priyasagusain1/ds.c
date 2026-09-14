6698ef9b18ffb4a9908aff901ac1193046edd012
priyasa gusain <gusainpriyasa1@gmail.com>
2026-09-14 17:59:45 +0000

    Add linked list C program

---
+++ linked_list.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// creating structure
+typedef struct node
+{
+    int info;
+    struct node *next;
+} node;
+
+// create node
+node *create_node()
+{
+    int value;
+    node *curr = (node *)malloc(sizeof(node));
+    if (curr == NULL)
+    {
+        printf("memory not allocated!!!");
+        return 0;
+    }
+    printf("enter value: ");
+    scanf("%d", &value);
+    curr->info = value;
+    return curr;
+}
+
+// case::1 [insert at left or beginning]
+node *left_insertion(node *head)
+{
+    node *curr;
+    curr = create_node();
+    curr->next = head;
+    head = curr;
+    return head;
+}
+
+// case::2 [insertion at right or ending]
+node *right_insertion(node *head)
+{
+    node *curr;
+    curr = create_node();
+    if (head == NULL)
+        return curr;
+    node *temp;
+    temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = curr;
+    curr->next = NULL;
+    return head;
+}
+
+// case::3 [insertion at between or at a position]
+node *insertion_at_position(node *head)
+{
+    int pos;
+    printf("enter a position you want to insert: ");
+    scanf("%d", &pos);
+    node *curr = create_node();
+    if (head == NULL)
+        return curr;
+    node *temp;
+    temp = head;
+    int i = 1;
+    if (pos == 1)
+    {
+
+        curr->next = head;
+        head = curr;
+    }
+    else
+    {
+        for (i = 1; i < pos - 1 && temp->next != NULL; i++)
+        {
+            temp = temp->next;
+        }
+        curr->next = temp->next;
+        temp->next = curr;
+    }
+    return head;
+}
+
+// printing linked list
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->info);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    head = left_insertion(head);
+    head = right_insertion(head);
+    head = insertion_at_position(head);
+    display(head);
+    return 0;
+}
